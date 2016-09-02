c02d5667585adef3d075a7e0e665a3d7ace82be2
Alison Paredes <ap2053@wildcats.unh.edu>
2016-09-02 09:14:08 -0400

    Option to search to horizon

--- src/bfs.py
+++ src/bfs.py
@@ -5,28 +5,38 @@ Created on Sep 1, 2016
 '''
 from collections import namedtuple
 
-def search(initial_state, goal_state):
+def search(initial_state, goal_state, horizon=float("inf")): 
     
     Node = namedtuple('Node',['state','previous','action']) #What kind of programming paradigm are factories? This is a little weird
     i = Node(initial_state, previous=None, action=None) #Are attributes implemented as copies or pointers?
     goal = Node(goal_state, previous=None, action=None)
-    open_list = []
-    open_list.append(i)
-    closed_list = [] #TODO: Use a hash table. How would I build a hash table? and hash function?
     
-    for s in open_list:
-        #print(open_list) #Is it helpful to see how the open list changes?
+    open_list = [i]
+    closed_list = [] #TODO: Use a hash table. How would I build a hash table? and hash function?
+    depth = 0 #TODO: Is there a good way to encapsulate depth
+    current_level = 1
+    next_level = 0
+    while len(open_list) > 0 and depth <= horizon:
+        s = open_list.pop(0)
+        current_level -= 1
         if is_goal(s,goal):
             return get_plan(s)    
         expanded = expand(s, Node)
-        add_open(open_list, closed_list, expanded)
-    return None #goal not found
+        next_level += add_open(open_list, closed_list, expanded)
+        if current_level == 0:
+            current_level = next_level
+            next_level = 0
+            depth += 1
+    return get_plan(s) #goal not found
 
 def add_open(open_list, closed_list, expanded): #Lists must be mutable
+    appended = 0
     for node in expanded:
         if node.state not in closed_list: #Tested by commenting "+ action" out of transition function for now
             open_list.append(node)
             closed_list.append(node.state)
+            appended += 1
+    return appended
 
 def is_goal(s, goal):
     if equals(s, goal): 
@@ -58,4 +68,5 @@ def equals(s1, s2):
     return s1.state == s2.state #TODO: Pass comparator for problem as a function
 
 if __name__ == '__main__':
+    print(1 <= float("inf"))
     pass
--- test/test_bfs.py
+++ test/test_bfs.py
@@ -20,7 +20,19 @@ class Test(unittest.TestCase):
         goal_state = 10
         plan=search(initial_state,goal_state)
         self.assertEquals(plan,[1,1,1,1,1,1,1,1,1],plan)
+        
+    def testSearch1toD9(self):
+        initial_state = 1
+        goal_state = 10
+        plan=search(initial_state,goal_state,9)
+        self.assertEquals(plan,[1,1,1,1,1,1,1,1,1],plan)
 
+    def testSearch1toD1(self):
+        initial_state = 1
+        goal_state = 10
+        plan=search(initial_state,goal_state,1)
+        self.assertEquals(plan,[1],plan)
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testSearch']
     unittest.main()
