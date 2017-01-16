a570994729198098ef7223036aa953660383f743
Alison Paredes <asparedes@gmail.com>
2017-01-16 10:48:54 -0500

    Regenerate food

--- src/agent.py
+++ src/agent.py
@@ -58,22 +58,6 @@ if __name__ == '__main__':
     parser.add_argument("time")
     args = parser.parse_args()
 
-
-    '''
-    # Scenario 1 used for development
-    # Agent knows about a food but there is another hidden food along the way. It should go directly to the food,
-    # stumble on the closer food and take that to the base with it for a reward.
-    initial_state = 'H--B\nF---\n----\n---F\n'
-    belief_state = 'H--B\n----\n----\n---F\n'
-    problem_spec = (4,4)
-    grid = problem.parse(initial_state)
-    grid_belief = problem.parse(belief_state)
-    State = namedtuple('State',['grid','reward'])
-    reward=0
-    belief_state=State(grid_belief, reward)
-    real_world=State(grid, reward, )
-    '''
-
     '''
     # Scenario 2 used for development
     # Agent must explore for food and take it to base. Simulator grows new food, so this scenario is endlessly
